3c03ee714775cc05160ee2ece22e8ec07dc5111a
Tyler O'Briant <tylerobriant@gmail.com>
2023-06-16 12:17:17 -0500

    cleanup. abstracted r/d into a generic, moved arg parsing for query eval into the query itself, not sure how i feel about it

--- plan.py
+++ plan.py
@@ -1,117 +1,294 @@
-from random import randint
-from datetime import date
-import os
+# okay in this one we're just going to make each function responsible for 
+# managing it's own file access.
 
-def UpsertPlan(planConfig):
-	OpenOrCreateFile(planConfig)
+import glob, os
+import re
+from datetime import date, timedelta
+from functools import reduce
+from line import CreateLine
 
-# okay so a user hits me with a command in the CLI
-# to remove friction, we'll just create a file if they don't have it
-# we'll let the know that happened
-# otherwise we'll just update the file
+PLANS_DIR = '{}/.plan'.format(os.getcwd())
 
-# what does plan config look like?
-# maybe something like
-# project name for now? and this is where we can hide all of the other goodies later
-# can edit this from the CLI and hide it in the bin? or maybe it's put in the plan folder
-# i think plan folder probably makes more sense, since it's the project settings
-# then we can have CLI settings that ARE stored in the bin
-
-# feels like the plan is the element we're interacting with via the API, not lines really
-# although we'll be able to add a line/lines from the CLI, those by default are editing today's plan
-
-# for now i want to keep the structure really simple, so we'll just assume that there are plan files in the same directory that the CLI is running in. I think it'd be cooler if the CLI searching for a .plan folder or something, or maybe just knew where projects were globally and could edit them.  Either way, a .plan folder with a bunch of files in seems right for clutter purposes.
-
-
-
-
-
-def OpenOrCreateFile(planConfig):
-	def buildFilename():
-		dateStr = date.today().strftime("%Y-%m-%d")
-		o = "{}.txt".format(dateStr)
-
-		return o
-
-
-	file_name = buildFilename()
-
-	if os.path.isfile(file_name):
-		# File exists, so open it
-		with open(file_name, "r") as file:
-			# Perform operations with the file
-			# For example, read its contents
-			contents = file.read()
-			print("File contents:", contents)
-				
-	else:
-		# File doesn't exist, so create it and write to it
-		with open(file_name, "w") as file:
-			# Write to the file
-			file.write("This is a new file.")
-
-		print("File created and written successfully.")
-	
-
-
-
-def openFile(filename):
-		return open(filename, "w+")
-
-def CreatePlan(filename):
-	# create the plan file and initialize it
-	pF = openFile(filename)
-
-	pF.write("Tyler O'Briant Plan File for today")
-
-	return pF
-
-def OpenPlan():
-	# plan name for now is plan_date.txt
-	def buildFilename():
-		dateStr = date.today().strftime("%Y-%m-%d")
-		o = "plan_{}.txt".format(dateStr)
-
-		return o
-
-	# we're assuming it's in this direct path with us right now, not at some config location or folder
-	def fileExists(filename):
+def fileExists(filename):
 		return os.path.isfile(filename)
 
+def buildFilename(dateStr):
+		return "plan_{}.txt".format(dateStr)
+
+# TODO this will need updated to actually only pull .plan files eventually
+def getAllPlanFilenames():
+	# little worried what happens when there are more
+	# than one root and dir arg here, the _
+	# can always fix later, it works now
+	for _, _, filenames in os.walk(PLANS_DIR):
+		return filenames
+
+
+def ListPlans(args):
+	print("list plans!")
+	# print(os.getcwd())
 	
+	for filename in getAllPlanFilenames():
+		print(filename) # some of this could probably be a bash command instead
+			# in fact, couldn't all this be a bash command? lol. we're just editing a text file
 
-	# okay some interesting notes is that the different file write modes might actually be useful here.
+def evaluateDateFromInput(query):
+		def regexLambda(p):
+			return lambda p, q: re.match(p, q)
 
-	# there is read-only, append only, write, write AND read
-	# so we may just not care about redundancy as much and treat this like a normal API yah know
+		print("query", query)
+		q = query[0].lower() if (query[0] not in ["", None]) else "today"
 
-	# have a router, have a service
-	filename = buildFilename()
+		rToday = r"^today$"
+		fToday = lambda q: date.today().strftime("%Y-%m-%d")
+		
+		rYesterday = r"^yesterday$"
+		fYesterday = lambda q: (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+		
+		rXAgo = r"^(\d+)\s+(day|week|month|year)s?\s+ago$"
+		def fXAgo(q):
+			rNum = int(re.match(r"^(\d+)", q).group(0))
+			rRange = re.search(r"(day|week|month|year)", q).group(0)
 
-	pF = openFile(filename) if (fileExists(filename)) else CreatePlan(filename)
+			scalars = {
+				"day": 1,
+				"week": 7,
+				"month": 30,
+				"year": 365
+			}
 
-	ReadPlan(pF)
+			scalar = scalars[rRange]
+			daysAgo = rNum*scalar
+			
+			return (date.today() - timedelta(days=daysAgo)).strftime("%Y-%m-%d")
+		
+		rXAgoShorthand = r"^-\d+[mwd](\d+[mwd])*"
+		def fXAgoShorthand(q):
+			matches = re.findall(r"\d+[mwd]", q)
+			scalars = {
+				"m": 30, 
+				"w": 7, 
+				"d": 1
+			}
+				
+			scaleAndSum = lambda sum, match: sum + int(match[:-1]) * scalars[match[-1]]
+			daysAgo = reduce(scaleAndSum, matches, 0)
+			
+			return (date.today() - timedelta(days=daysAgo)).strftime("%Y-%m-%d")
+
+		rDateString = r"^(?:(\d{4})-\d{2}-\d{2}|\d{2}-\d{2}-(\d{4}))$"
+		def fDateString(q):
+			x = q.split("-")
+
+			if (len(x[0]) == 4):
+				return q
+
+			return "{}-{}-{}".format(x[2], x[1], x[0])
+			
+		rDayOfWeek = r"\b(?:monday|tuesday|wednesday|thursday|friday|saturday|sunday|m|t|w|r|f|s|u)\b"
+		def fDayOfWeek(q):
+			match = re.search(rDayOfWeek, q)
+
+			extractedDay = q[match.start():match.end()]
+			print(extractedDay)
+			
+			referenceDate = date.today() if ("last" not in q) else (date.today() - timedelta(days=7))
+			referenceWeekday = referenceDate.weekday()
+			days_of_week = [
+				("monday", "m"),
+				("tuesday", "t"),
+				("wednesday", "w"),
+				("thursday", "r"),
+				("friday", "f"),
+				("saturday", "s"),
+				("sunday", "u")
+			]
+
+			# print('filter', filter(lambda d: d[0] == q or d[1], days_of_week)[0])
+			# print('days_of_week.index()', days_of_week.index(filter(lambda d: d[0] == q or d[1], days_of_week)[0]))
+
+			# next((x for x in array if x % 2 == 0), None)
+			target_weekday = next((d for d in days_of_week if d[0] == extractedDay or d[1] == extractedDay), 'monday')
+			target_weekday_index = days_of_week.index(target_weekday)
+
+			if referenceWeekday == target_weekday_index:
+				nearest_day = referenceDate - timedelta(weeks=1)
+			elif referenceWeekday > target_weekday_index:
+				difference = referenceWeekday - target_weekday_index
+				nearest_day = referenceDate - timedelta(days=difference)
+			else:
+				difference = target_weekday_index - referenceWeekday
+				nearest_day = referenceDate - timedelta(days=difference + 7)
+
+			return nearest_day.strftime("%Y-%m-%d")
+		
+		cases = [
+			(
+				rToday, # done
+				fToday
+			),
+			(
+				rYesterday, # done
+				fYesterday
+			),
+			(
+				rXAgo, # done? -> yes
+				fXAgo
+			),
+			(
+				rXAgoShorthand, # done? -> yes
+				fXAgoShorthand
+			),
+			(
+				rDateString, # done? -> yes
+				fDateString				
+			),
+			(
+				rDayOfWeek, # done? -> yes
+				fDayOfWeek
+			)
+		]
+
+		for pattern, procedure in cases:
+			if re.search(pattern, q):
+				return procedure(q)
+
+		return "default" # TODO return error
+
 	
-	pF.close()
+# Okay so right now there is a pattern of "Access File", where I have some procedure I want to run on a file, and I have some date I need to build that filename
+# if that file doesn't exist
+def accessFile(filename, fileOperationProc, fileDoesNotExistProc):
+	if not fileExists(filename):
+		returnCode = fileDoesNotExistProc(filename)
+		
+		if (returnCode not in [0, None]):
+			return
+		
+	
+	fileOperationProc(filename)
 
-	# in here we're going to look to see if we have a plan file for today
-	# if we do, we'll return it
-	# if we do not, we'll create it, initialize it, and then return it
+def ReadPlan2(args):
+	# for now, if there is nothing at the corresponding date, then we'll just say so
+	# eventually I want a "def findNearest" that will give you the option to scan for the nearest date and pull that up
+	def readFile(filename):
+		print("\n\n")
+		print("reading {}:\n".format(filename))
+		with open(filename, "r") as file:
+			contents = file.read()
+			print(contents)
+		print("\n\n")
 
-#def UpdatePlan():
-	# update plan file
-	# will take in 
+	def doesNotExist(_):
+		print("hey that doesn't exist!")
+		return 1	
 
-#def DeletePlan():
-	# delete the file
+	targetDate = evaluateDateFromInput(args)
+	filename = buildFilename(targetDate)
+	
+	accessFile(
+		filename, 
+		readFile, 
+		doesNotExist
+	)
+	return 0
 
-def ReadPlan(file):
-	# will simply read the plan file
-	for line in file:
-		print(line)
-
-#def FormatPlan():
-	# will apply formatting and overwrite the plan file's current formatting
 
 
+
+
+def ReadPlan(args):
+	# for now, if there is nothing at the corresponding date, then we'll just say so
+	# eventually I want a "def findNearest" that will give you the option to scan for the nearest date and pull that up
+	def readFile(filename):
+		print("\n\n")
+		print("reading {}:\n".format(filename))
+		with open(filename, "r") as file:
+			contents = file.read()
+			print(contents)
+		print("\n\n")
+
+
+	targetDate = evaluateDateFromInput(args)
+	filename = buildFilename(targetDate)
+	
+	if not fileExists(filename):
+		# throw a real error
+		print("hey that doesn't exist!")
+		return 1
+
+	readFile(buildFilename(targetDate))
+	return 0
+
+def SearchPlans(args):
+	print("search plans!")
+
+def UpsertPlan(args):
+	def createPlan(filename):
+		formattedDate = date.today().strftime("%B %d, %Y")
+		headerText = ".plan for {}".format(formattedDate)
+		headerDashes = "-"*len(headerText)
+		header = "{}\n{}\n{}\n".format(headerDashes, headerText, headerDashes)
+
+		with open(filename, "w") as f:
+			f.write(header)
+
+	
+	def updatePlan(filename, update):
+		with open(filename, "a") as f:
+			f.write(update)
+
+	# TODO check for args existing correctly
+	lineType, text = args
+
+	##
+	#	Find today's file, if it does not exist, create it
+	##
+	today = date.today().strftime("%Y-%m-%d")
+	filename = buildFilename(today)
+
+
+	if not fileExists(filename):
+		createPlan(filename)
+
+
+	##
+	#	Format the line, then append
+	##
+	update = CreateLine(lineType, text)
+	updatePlan(filename, update)
+		
+	# TODO return code
+	return 0
+
+# note, this has the exact same structure as read plan
+def DeletePlan2(args):
+	def deleteFile(filename): 
+		print("deleting {}".format(filename))
+		os.remove(filename)
+	
+	def ifDoesNotExist(_):
+		print("hey that doesn't exist!")
+		return 1
+
+	targetDate = evaluateDateFromInput(args)
+	filename = buildFilename(targetDate)
+	
+	accessFile(filename, deleteFile, ifDoesNotExist)
+
+# note, this has the exact same structure as read plan
+def DeletePlan(args):
+	def deleteFile(filename): 
+		print("deleting {}".format(filename))
+		os.remove(filename)
+
+	targetDate = evaluateDateFromInput(args)
+	filename = buildFilename(targetDate)
+	
+	if not fileExists(filename):
+		# throw a real error
+		print("hey that doesn't exist!")
+		return 1
+
+	deleteFile(filename)
 
--- router.py
+++ router.py
@@ -1,28 +1,23 @@
-import plan2
+import plan
 
-# here we're going to take in all of our CLI commands and parse them out to the right functions
-
-# can make a CMD enum if we care enough
-
-# lots of interesting stuff we can do in here https://learnpython.com/blog/python-match-case-statement/
 def Router(cmd, args):
 	match (cmd):
 		case "r" | "read":
-			return plan2.ReadPlan(args)
+			return plan.ReadPlan2(args)
 
 		case "w" | "write":
-			return plan2.UpsertPlan(args)
+			return plan.UpsertPlan(args)
 
 		case "s" | "search":
-			return plan2.SearchPlans(args)
+			return plan.SearchPlans(args)
 
 		case "d" | "delete":
-			return plan2.DeletePlan(args)
+			return plan.DeletePlan2(args)
 
 		case "l" | "list":
-			return plan2.ListPlans(args)
+			return plan.ListPlans(args)
 
 		case _:
-			# throw an error
+			# TODO throw an error
 			print("router!")
 		
